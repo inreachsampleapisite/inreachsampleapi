dbaa511665e9728fe86a7c8f847e6d7ae70cbdc4
Joshua Le <joshua.le@garmin.com>
2017-10-10 13:50:26 -0400

    First Master Commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(inreachsampleapi.Startup))]
+namespace inreachsampleapi
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
